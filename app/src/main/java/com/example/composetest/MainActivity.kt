0a28a73cb71bf801ecedf7b9f3ca07c5825b8412
ts moosavi <tt.musavi@gmail.com>
2023-03-18 16:18:00 +0330

    add alignment and arrangement

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -4,19 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composetest.ui.theme.ComposeTestTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 
 class MainActivity : ComponentActivity() {
@@ -41,9 +46,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayGreetingWithText(modifier: Modifier = Modifier, message: String, from: String) {
     Surface(color = Color.Red) {
-        Column(modifier = modifier) {
-            Text(text = message, fontSize = 36.sp)
-            Text(text = from, fontSize = 24.sp)
+        Column(modifier = modifier.background(color = Color.Magenta),verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = message, fontSize = 36.sp, modifier = Modifier.padding(20.dp))
+            Text(text = from, fontSize = 24.sp, modifier = Modifier.padding(10.dp))
         }
     }
 }
@@ -71,7 +76,7 @@ fun Greeting(name: String) {
 fun  BirthdayGreetingWithImage(message: String, from: String,modifier: Modifier = Modifier){
 val picture = painterResource( R.drawable.androidparty)
     Box{
-        Image(painter = picture, contentDescription = null)
+        Image(painter = picture, contentDescription = null, contentScale = ContentScale.Crop)
                             BirthdayGreetingWithText(message = "hi baby", from = "tt")
     }
 }
